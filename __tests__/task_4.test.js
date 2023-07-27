d785375ebcefa989993e14f534a59681d5d39669
GeekyHacks <geekyhacks22@gmail.com>
2023-07-27 18:25:50 +0300

    Edit task_4 and its tests

--- __tests__/task_4.test.js
+++ __tests__/task_4.test.js
@@ -1,33 +1,15 @@
-// import { capital } from '../tasks/task_4.js';
+import  capital  from '../tasks/task_4.js';
 
-const capital = (str) => {
-  return str[0].toUpperCase() + str.slice(1);
-};
+test('String is defined', () => {
+  expect(capital('hello')).toBeDefined;
+});
 
-let expected;
-let input;
-
-let result = capital(input);
-// Test 1: Check if the first character is capitald
 test('capital the first character of the string', () => {
-  input = 'hello';
-  expected = 'Hello';
-
-  expect(result).toBe(expected);
+  expect(capital('hello')).toBe('Hello');
 });
 
 // Test 2: Check if already capitald string remains the same
 test('already capitald string should remain the same', () => {
-  input = 'Hello';
-  expected = 'Hello';
-
-  expect(result).toBe(expected);
+  expect(capital('Hello')).toBe('Hello');
 });
 
-// Test 3: Check if an empty string remains empty
-test('empty string remains empty', () => {
-  input = '';
-  expected = '';
-
-  expect(result).toBe(expected);
-});
--- tasks/task_4.js
+++ tasks/task_4.js
@@ -1,3 +1,4 @@
-export const capital = (str) => {
+export default capital = (str) => {
   return str[0].toUpperCase() + str.slice(1);
 };
+// console.log(capital('you'));
