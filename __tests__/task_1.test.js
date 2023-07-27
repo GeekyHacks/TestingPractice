00a42c3f66f02e7950b3e09a673d12029359a6da
GeekyHacks <geekyhacks22@gmail.com>
2023-07-27 09:11:05 +0300

    Add task_1 with its test file

---
+++ __tests__/task_1.test.js
@@ -0,0 +1,5 @@
+import { strLength } from '../tasks/task_1';
+
+    test('return the length of string', () => {
+  expect(strLength(String)).toBe(String.length);
+});
--- tasks/task_1.js
+++ tasks/task_1.js
@@ -1,8 +1,15 @@
 // string length counter
 
-const strLength = (str) => {
-  return str.length;
+export const strLength = (String) => {
+  const result = String.length;
+  for (let i = 0; i < result; i++) {
+    if (i > 10) {
+      return 'string is greater than 10';
+    }
+  }
+
+  return result;
 };
 
-const a = 'you are the best';
-console.log(strLength(a));
+// const a = 'you are the best';
+// console.log(strLength(a));
